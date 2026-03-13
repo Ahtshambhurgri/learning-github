baa1a1fb596f131599ca41c2aacced6008b5eba0
Ahtsham Bhurgri <bhurgriahtsham@gmail.com>
2026-03-13 10:53:39 +0500

    Student file added with ready state

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld{
         System.out.println("Hello, World!");
         System.out.println("Welcome to Java programming.");
         System.out.println("Date is "+ new Date());
-        
+        Student student1 = new Student("Alice", 20, "A");
+        System.out.println("Student Name: " + student1.getName());
+        System.out.println("Student Age: " + student1.getAge());
+        System.out.println("Student Grade: " + student1.getGrade());
     }
 }
