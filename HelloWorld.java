57a6d3e9b0fedb7f0d597b566e57a398092d4b2d
Ahtshambhurgri <133470953+Ahtshambhurgri@users.noreply.github.com>
2026-03-14 18:14:35 +0500

    Correct typo in date output statement
    Fixed a typo in the print statement for the date.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to Java programming.");
-        System.out.println("Date is "+ new Date());
+        System.priln("Date is "+ new Date());
         Student student1 = new Student("Alice", 20, "A");
         System.out.println("Student Name: " + student1.getName());
         System.out.println("Student Age: " + student1.getAge());
